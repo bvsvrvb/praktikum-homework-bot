d3945625eec00812097558a67404da78843ae82c
Vlad Bashmakov <thecherage@gmail.com>
2022-12-29 17:10:41 +0100

    pep8

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,18 @@
 class TimestampException(Exception):
     pass
 
+
 class BadTokenException(Exception):
     pass
+
 
 class NotOKResponse(Exception):
     pass
 
+
 class NoHomeworkName(Exception):
     pass
+
 
 class NoStatusData(Exception):
     pass
